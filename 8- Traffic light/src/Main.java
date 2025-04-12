51fa0cc4c60dac395761f5ef7f7ade1055aad414
Beshoy Farag <beshoynady401@gmail.com>
2025-04-12 12:07:26 +0200

    finish the traffic light

--- 8- Traffic light/src/Main.java
+++ 8- Traffic light/src/Main.java
@@ -1,7 +1,13 @@
+/*
+Write a Java program to create class called "TrafficLight" with attributes for color and duration,
+and methods to change the color and check for red or green.
+*/
+
+
 public class Main {
     public static void main(String[] args) {
-        TrafficLight tl = new TrafficLight();
-
+        TrafficLight tl = new TrafficLight("green", 30);
+        tl.greenLight();
         tl.startTrafficLight();
     }
 }
--- 8- Traffic light/src/TrafficLight.java
+++ 8- Traffic light/src/TrafficLight.java
@@ -1,25 +1,75 @@
+/*
+*  Write a Java program to create class called "TrafficLight" with attributes for color and duration,
+* and methods to change the color and check for red or green.
+* */
+
+
 public class TrafficLight {
+    private int duration;
+    private String color;
+
+
+
+    public TrafficLight(String color, int duration){
+        this.color = color;
+        this.duration = duration;
+    }
+
+    public boolean redLight(){
+        return color.equalsIgnoreCase("red");
+    }
+    public boolean greenLight() {
+        return color.equalsIgnoreCase("green");
+    }
+    public boolean yellowLight() {
+        return color.equalsIgnoreCase("yellow");
+    }
 
 
     public void startTrafficLight() {
 
         while (true) {
-            System.out.println("Red Light Stop!");
-            holdColor(30);
-            System.out.println("Yellow Light!");
-            holdColor(5);
-            System.out.println("Green Light!");
-            holdColor(60);
+            if (redLight()) {
+                System.out.println("Red light you can not pass!");
+                holdColor(duration);
+                System.out.println("Yellow Light!");
+                holdColor(5);
+                System.out.println("Green Light you can go!");
+                holdColor(60);
+            }
+            else if (yellowLight()) {
+                System.out.println("Yellow Light!");
+                holdColor(duration);
+                System.out.println("Green Light you can go!");
+                holdColor(60);
+                System.out.println("Yellow Light!");
+                holdColor(duration);
+                System.out.println("Red light you can not pass!");
+                holdColor(45);
+
+            }
+            else if (greenLight()) {
+                System.out.println("Green Light you can go!");
+                holdColor(duration);
+                System.out.println("Yellow Light!");
+                holdColor(5);
+                System.out.println("Red light you can not pass!");
+                holdColor(40);
+            }
+
+
+
+
         }
 
     }
 
 
 
-    public void holdColor(int seconds){
+    public void holdColor(int duration){
 
         try {
-            Thread.sleep(seconds * 1000);
+            Thread.sleep(duration * 1000);
         }
         catch(Exception e){
             System.out.println("Traffic Light bug");
